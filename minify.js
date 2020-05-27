d84d561c747f5252cb532f7521b33ba5bd140adf
Dan Moneypenny <catullus1973@gmail.com>
2020-05-27 00:40:56 -0700

    add minified version

--- minify.js
+++ minify.js
@@ -4,7 +4,7 @@ const gcc = require('@node-minify/google-closure-compiler');
 const uglifyJS = require('@node-minify/uglify-js')
 
 minify({
-  compressor: gcc,
+  compressor: uglifyJS,
   input: 'public/js/app.js',
   output: 'public/js/app-min.js',
   callback: function(err, min) {
